cee52d41270e656c80efd6397be8411a46e4f3c3
NallaAnjani <anjanikumarinalla@gmail.com>
2025-05-08 20:04:36 +0530

    frontend

---
+++ src/App.js
@@ -0,0 +1,61 @@
+// import React, { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//   const [inr, setInr] = useState('');
+//   const [result, setResult] = useState({ usd: '', eur: '' });
+
+//   const convert = async () => {
+//     const res = await fetch(`/api/convert?amount=${inr}`);
+//     const data = await res.json();
+//     setResult(data);
+//   };
+  
+
+//   return (
+  
+//     <div className="App">
+//       <h2>INR to USD/EUR Converter</h2>
+//       <label>Enter your emount :</label>
+//       <input type="number" placeholder="Enter INR" value={inr} onChange={(e) => setInr(e.target.value)}/><br/>
+//       <button onClick={convert}>Convert</button>
+//       <h3>USD: {result.usd}</h3>
+//       <h3>EUR: {result.eur}</h3>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [inr, setInr] = useState('');
+  const [result, setResult] = useState({ usd: '', eur: '' });
+
+  const convert = async () => {
+    const res = await fetch(`https://currency-backend-rose.vercel.app/api/convert?amount=${inr}`);
+    // const res = await fetch(`/api/convert?amount=${inr}`);
+    const data = await res.json();
+    setResult(data);
+  };
+
+  return (
+  
+    <div className="App">
+      <h2>INR to USD/EUR Converter</h2>
+      <label>Enter your emount :</label>
+      <input type="number" placeholder="Enter INR" value={inr} onChange={(e) => setInr(e.target.value)}/><br/>
+      <button onClick={convert}>Convert</button>
+      <h3>USD: {result.usd}</h3>
+      <h3>EUR: {result.eur}</h3>
+    </div>
+  );
+}
+
+export default App;
+
+
